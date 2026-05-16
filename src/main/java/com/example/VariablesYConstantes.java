e508323e85b612cddc5d76fc38d95573e9147430
nannyparra-lang <nannyparra@gmail.com>
2026-05-16 16:28:03 +0000

    cambios ejercio 1

--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -3,6 +3,10 @@ package com.example;
 public class VariablesYConstantes {
     public static void demostrarDeclaracionAsignacion() {        
         // TODO
+        int edad = 20
+        double estatura = 1.65
+        System.out.println(edad);
+        System.out.println(estatura);
     }
 
     public static void demostrarConvencionesNombres() {
