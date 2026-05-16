2a8de05313752d10567f26d8b1b2ecd3ccecec76
nannyparra-lang <nannyparra@gmail.com>
2026-05-16 20:46:57 +0000

    ejercicio4

--- src/main/java/com/example/Operadores.java
+++ src/main/java/com/example/Operadores.java
@@ -3,26 +3,33 @@ package com.example;
 public class Operadores {
     public static void demostrarAritmeticos() {
         // TODO
+        double nota1 = 4.2;
+        double nota2 = 3.8;
+        double nota3 = 5.0;
+
+        double promedio = (nota1 + nota2 + nota3) / 3
         
     }
 
     public static void demostrarIncrementoDecremento() {
         // TODO
+        int = 5;
+
+        System.out.println(++n);
+        System.out.println(n++);
     }
 
-    public static void demostrarRelacionales() {
-        // TODO
-    }
 
     public static void demostrarLogicos() {
         // TODO
+        int = 30;
+        System.out.println(numero > 10 && numero < 50);
     }
 
-    public static void demostrarAsignacionCompuesta() {
-        // TODO
-    }
 
     public static void demostrarOperadorTernario() {
         // TODO
+        int = -8
+        String resultado = (numero >=0) ? "verdadero" : "falso";
     }
 }
